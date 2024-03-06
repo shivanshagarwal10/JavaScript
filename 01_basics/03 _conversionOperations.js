e9fe469f144ceb2f8953182dc992b39200d8054a
shivanshagarwal10 <119075715+shivanshagarwal10@users.noreply.github.com>
2024-03-06 21:42:39 +0000

    add some more comments

--- 01_basics/03 _conversionOperations.js
+++ 01_basics/03 _conversionOperations.js
@@ -22,9 +22,58 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 let someNumber = 10;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ***********************Operation**********************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 = "hello";
+let str2 = " shivansh";
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(true);
+// console.log(false);
+// console.log(+"");
+
+let num1, num2, num3 
+let gameCounter = 100;
+num1 = num2 = num3 = 2 + 2
+++gameCounter;
+//console.log(gameCounter);
 
 
 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
 
 
