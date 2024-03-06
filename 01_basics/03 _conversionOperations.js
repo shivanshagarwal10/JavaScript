52468b88245914123ea9ab406d71e7a6f81f34b3
shivanshagarwal10 <119075715+shivanshagarwal10@users.noreply.github.com>
2024-03-06 21:01:46 +0000

    Conversion of data types

---
+++ 01_basics/03 _conversionOperations.js
@@ -0,0 +1,30 @@
+/*let score = true;
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// "33" => 33;
+// "33abc" => NaN;
+// true => 1; false => 0;
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+
+// "" => false;
+// "shivansh" = true;
+// 1 => true; 0 => false;
+
+let someNumber = 10;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
