094360c87fb1f7be58bf674f03dcd4daf55ca27d
shivanshagarwal10 <119075715+shivanshagarwal10@users.noreply.github.com>
2024-03-10 07:43:49 +0000

    Math and nums bascis and their behaviour

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.77385
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//***************************MATHS****************************//
+
+// console.log(Math);
+// console.log(Math.max(5,6));
+// console.log(Math.min(5,6,2,3));
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(3.8));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+//Important
+//***console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
