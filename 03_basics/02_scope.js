d18c9c9d528ee42771715d8ae51b9076767e66db
shivanshagarwal10 <119075715+shivanshagarwal10@users.noreply.github.com>
2024-04-12 06:42:40 +0000

    scope basic

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,20 @@
+// var c = 300
+let a = 300
+
+
+if(true){
+    let a = 10
+    const b = 20
+
+    console.log("INNER:", a);
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+//console.log(a);
+//console.log(b);
+console.log(a);
+// we don't declare the var because it just print the value in the function and the outer also;
